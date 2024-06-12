720111474905b2331561b39c1ae7cc80c58c4208
Arina <arina.shabelski@gmail.com>
2024-06-12 14:03:35 +0400

    Первая задача

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿void PrintNaturalNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write(m + " ");
+        PrintNaturalNumbers(m + 1, n);
+    }
+}
+
+    Console.Write("Введите значение M: ");
+    int m = int.Parse(Console.ReadLine());
+
+    Console.Write("Введите значение N: ");
+    int n = int.Parse(Console.ReadLine());
+
+    Console.Write("Натуральные числа в диапазоне от M до N: ");
+    PrintNaturalNumbers(m, n);
+
+    Console.WriteLine();
